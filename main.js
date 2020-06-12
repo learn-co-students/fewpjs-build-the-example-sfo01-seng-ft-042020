ee8317822db123dada62fa9b43f1e65b838a1eed
github username <you@example.com>
2020-06-11 18:45:01 -0700

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,56 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// hide error model
+let modal = document.getElementById("modal");
+modal.setAttribute("class", "hidden");
 
+document.addEventListener("DOMContentLoaded", function() {
+  heartClickListener();
+})
 
+function heartClickListener() {
+    // get all hearts
+    const heartButtons = document.querySelectorAll(".like");
+    // iterate over all hearts - extract single heart
+    heartButtons.forEach((heart) => {
+      // add click listener to a single heart
+      heart.addEventListener("click", function(event) {
+        // make sure only button is clickable
+        if (event.target.className === "like-glyph") {
+          // invoke mimicServerCall to simulate making a server request
+          mimicServerCall()
+            // if successful
+            .then((resolve) => {
+              console.log(resolve)
+              // make full heart
+              let fullHeart = event.target;
+              fullHeart.setAttribute("class", "activated-heart");
+              fullHeart.innerHTML = FULL_HEART;
+              // unlike - turn full heart into empty heart when clikced
+              fullHeart.addEventListener("click", function(event) {
+                event.target.removeAttribute("class");
+                event.target.innerHTML = EMPTY_HEART;
+              })
+            })
+            // if not successful
+            .catch((reject) => {
+              console.log(reject)
+              // show modal
+              modal.classList.remove("hidden");
+              // display reject message in modal
+              const h2 = document.getElementById("error-message");
+              h2.innerText = reject;
+              // hide the modal after 5 seconds
+              setTimeout(function() {
+                modal.setAttribute("class", "hidden")
+              }, 5000)
+            });
+        }
+      });
+
+    })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
